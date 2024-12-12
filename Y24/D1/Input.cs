1599cbc179ccd9e653dc8f25bbe31e0221924bbd
Ollie Knox <ollie.knox@outlook.com>
2024-12-12 21:00:49 +0000

    Add project files.

---
+++ Y24/D1/Input.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode.Y24.D1
+{
+    class Input
+    {
+        public List<int> Left { get; set; } = [];
+        public List<int> Right { get; set; } = [];
+
+        public override string ToString()
+        {
+            return $"{{\n Left: [\n{string.Join(",", Left)}\n],\n Right: [\n{string.Join(",", Right)}\n]\n }}";
+        }
+    }
+}
---
+++ Y24/D1/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using AdventOfCode.Y24.D1;
+
+var executablePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new DirectoryNotFoundException("Unable to find directory of executable");
+var inputPath = Path.Combine(executablePath, @"Y24/D1/Input.txt");
+
+var lines = File.ReadAllLines(inputPath).ToList();
+
+var input = new Input();
+
+lines.ForEach(line =>
+{
+    var parsedLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    Debug.Assert(parsedLine.Length == 2); 
+
+    input.Left.Add(int.Parse(parsedLine[0]));
+    input.Right.Add(int.Parse(parsedLine[1]));
+});
+
+Part1();
+Part2();
+
+void Part1()
+{
+    Console.WriteLine("2024 | Day 1 | Part 1");
+
+    input.Left.Sort();
+    input.Right.Sort();
+
+    Debug.Assert(input.Left.Count == input.Right.Count);
+
+    var distances = new List<int>();
+
+    for (var i = 0; i < input.Left.Count; i++)
+    {
+        var left = input.Left[i];
+        var right = input.Right[i];
+        var distance = Math.Abs(left - right);
+        distances.Add(distance);
+    }
+
+    var sum = distances.Sum();
+
+    Console.WriteLine($"Answer: {sum}");
+}
+
+void Part2()
+{
+    Console.WriteLine("2024 | Day 1 | Part 2");
+
+    var countPerLocation = new Dictionary<int, int>();
+
+    input.Left.ForEach(location =>
+    {
+        countPerLocation.Add(location, 0);
+    });
+
+    input.Right.ForEach(location =>
+    {
+        if (countPerLocation.TryGetValue(location, out var count))
+        {
+            countPerLocation[location]++;
+        }
+    });
+
+    var sum = input
+        .Left
+        .Select(location =>  location * countPerLocation[location])
+        .Sum();
+
+    Console.WriteLine($"Answer: {sum}");
+}
